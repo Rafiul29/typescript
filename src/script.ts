622a292783372be2c5fb53619a9924f1e71d1edb
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-06 14:51:58 +0600

    typescript basic

---
+++ src/script.ts
@@ -0,0 +1,14 @@
+// console.log("Hello Rafiul")
+
+// const country = "Bangadesh is very ";
+// console.log(country)
+
+
+// function multiply(a:number,b:number){
+//     return a*b
+// }
+
+// console.log(multiply(6,4))
+
+// const array =["rafi","rakib","shamim"]
+// array.push(true)
