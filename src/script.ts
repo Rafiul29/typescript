e4c05c74bf9de4b32833059f34b8a83228bbe67b
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-07 00:59:22 +0600

    abstraction description

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,10 @@
+// abstraction helps us to hide the implementation of something
+//
+// class declared with abstract keyword
+//
+// object can not be created from abstract class
+//
+// if a class extends abstract class; it must inherit all the abstract methods
 
 abstract class User{
     name: string;
