bc5d949f86cfbf56d9a800c018fe96ac07a4734d
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-06 21:08:19 +0600

    Class and object

--- output/script.js
+++ output/script.js
@@ -1,61 +1,15 @@
 "use strict";
-// Array types
-var userName;
-userName = ['rafi', 'rakib'];
-console.log(userName[0]);
-var multipleTypes;
-multipleTypes = ['hjs', 34];
-//Dynamic Types
-var a = [];
-a.push('hsjs');
-a.push('566');
-//Object types
-// let c:{
-//     name: string
-//     age: number
-// }
-// c={
-//     name: "rafiul islam",
-//     age: 67
-// }
-// just object declaration
-var users;
-users = {
-    'id': 1,
-    name: 'jnjndjn'
-};
-//object array declaration
-var person;
-person = [{
-        'id': 1,
-        'name': "rafiul"
-    },
-    {
-        'id': 2,
-        'name': 'rakibul'
+var User = /** @class */ (function () {
+    function User(n, a) {
+        this.name = n;
+        this.age = a;
     }
-];
-var person1;
-person1 = { 'id': 3, 'name': "shamim" };
-person.push(person1);
-var person2;
-person2 = { 'id': 2011137, 'name': 'Asfia' };
-person.push(person2);
-for (var key in person) {
-    console.log(person[key]);
-}
-var friends;
-friends = [];
-var friend1;
-friend1 = { 'id': 212, 'name': 'Rafi' };
-friends.push(friend1);
-var friend2;
-friend2 = { 'id': 2011137, 'name': 'Asfia Hossen Mohona' };
-friends.push(friend2);
-console.log(friends);
-var getRequest;
-getRequest = "GET";
-function requestHandler(requestType) {
-    console.log(requestType);
-}
-requestHandler("GET");
+    User.prototype.disPlay = function () {
+        console.log("name " + this.name + " age " + this.age);
+    };
+    return User;
+}());
+var User1 = new User("Rafiul", 24);
+User1.disPlay();
+var User2 = new User("Asfia Hossen", 21);
+User2.disPlay();
--- src/script.ts
+++ src/script.ts
@@ -1,74 +1,18 @@
-// Array types
-let userName: string[];
-userName=['rafi','rakib'];
-console.log(userName[0])
-let multipleTypes: (string | number) [];
-multipleTypes= ['hjs', 34];
 
-//Dynamic Types
-let a: any[]=[]
-a.push('hsjs');
-a.push('566');
-
-//Object types
-// let c:{
-//     name: string
-//     age: number
-// }
-// c={
-//     name: "rafiul islam",
-//     age: 67
-// }
-
-// just object declaration
-let users: object;
-users ={
-    'id': 1,
-    name: 'jnjndjn'
-}
-
-//object array declaration
-let person: object[];
-    person=[{
-        'id': 1,
-        'name': "rafiul"
-    },
-    {
-        'id': 2,
-        'name': 'rakibul'
+class User{
+    name: string;
+    age: number;
+    constructor(n:string,a:number) {
+        this.name=n;
+        this.age=a;
     }
-]
-let person1:{id: number,name: string}
-person1={'id':3, 'name': "shamim"}
-person.push(person1)
-
-let person2:{id: number,name: string}
-person2={'id':2011137, 'name': 'Asfia'}
-person.push(person2)
-for(const key in person){
-    console.log(person[key]);
+    disPlay(): void{
+        console.log(`name ${this.name} age ${this.age}`);
+    }
 }
 
+const User1 = new User("Rafiul",24);
+User1.disPlay();
 
-//custom type
-
-type user= {id: number, name: string}
-
-let friends: user[];
-friends=[]
-let friend1: user;
-friend1={'id':212, 'name': 'Rafi' }
-friends.push(friend1)
-let friend2: user;
-friend2={'id':2011137, 'name': 'Asfia Hossen Mohona' }
-friends.push(friend2)
-console.log(friends)
-
-type RequestType ="GET" | "POST";
-let getRequest: RequestType;
-getRequest="GET"
-
-function requestHandler(requestType: RequestType){
-    console.log(requestType)
-}
-requestHandler("GET")
+const User2 = new User("Asfia Hossen",21);
+User2.disPlay();
