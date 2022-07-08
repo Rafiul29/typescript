c65023d19acb842f649d4a37298075d4b1f9314c
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-08 15:34:45 +0600

    dom manipulation

--- src/script.ts
+++ src/script.ts
@@ -1,39 +1,21 @@
-//Encapsulation....
-//Access modifiers : public,privet,protected,readonly
-class User{
-   public name: string;
-   public age: number;
-    constructor(n:string,a:number) {
-        this.name=n;
-        this.age=a;
-    }
-    disPlay(): void{
-        console.log(`name ${this.name} age ${this.age}`);
-    }
-}
-class Students extends User{
-   private id: number;
-    constructor(n:string,a:number,id:number) {
-        super(n,a);
-        this.id=id;
-    }
-    disPlay(): void{
-        console.log(`name ${this.name} age ${this.age} id: ${this.id}`);
-    }
-    setId(id: number): void{
-        this.id=id;
-    }
-    getId(): number{
-        return this.id;
-    }
-}
-// const  user1 = new User("rafiul", 24)
-// user1.name='asfia'
-// console.log(user1)
+const form = document.querySelector(".user-form") as HTMLFormElement
+console.log(form)
 
+const firstNameInput= document.querySelector("#firstname") as HTMLInputElement;
+const lastNameInput = document.querySelector("#lastname") as HTMLInputElement;
+const userNameInput= document.querySelector("#username") as HTMLInputElement;
+console.log(userNameInput)
+const selectInput = document.querySelector("#country") as HTMLSelectElement;
+const feedbackInput = document.querySelector("#feedback") as HTMLTextAreaElement;
 
-const Student1 = new Students('Rafiul',24,1814029)
-const Student2 = new Students('Asfia',22,2011137)
-Student1.disPlay();
-Student1.setId(123)
-console.log(Student1.getId())
+form.addEventListener("submit",(e:Event)=>{
+    e.preventDefault();
+    let userData={
+        firstname: firstNameInput.value,
+        lastname: lastNameInput.value,
+        userName: userNameInput.value,
+        country: selectInput.value,
+        feedback: feedbackInput.value
+    }
+    console.log(userData)
+})
